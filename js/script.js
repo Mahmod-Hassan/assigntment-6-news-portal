c62d67c80790407387bb804bc048321ec5197617
Mahmod Hassan <mahmodhasan7788@gmail.com>
2022-09-03 14:58:23 +0600

    author, review and ratings complete

--- js/script.js
+++ js/script.js
@@ -14,7 +14,9 @@ const displayCategoryButton = (categories) => {
         categoryButtonContainer.appendChild(div);
     })
 
-}
+};
+
+
 const getCategoryById = async (category_id) => {
     toggleSpinner(true);
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
@@ -23,24 +25,49 @@ const getCategoryById = async (category_id) => {
     displayCategories(data.data);
 }
 const displayCategories = (categoryItem) => {
-    console.log(categoryItem);
     const categoryContainer = document.getElementById('categories-container');
     categoryContainer.textContent = ``;
     if (categoryItem.length === 0) {
         dataNotFound(categoryContainer);
     }
     categoryItem.forEach(item => {
+        console.log(item);
         const div = document.createElement('div');
+        div.style.backgroundColor = 'white';
         div.innerHTML = `
-            <div class="row bg-light p-3">
-               <div class="col-3">
+            <div class="row p-3 mb-5">
+               <div class="col-sm-12 col-lg-3">
                     <img src="${item.thumbnail_url}" class="img-fluid rounded-start w-100" alt="...">
                 </div>
-                <div class="col-9">
-                    <div">
+                <div class="col-sm-12 col-lg-9">
+                    <div class='align-content-center'>
                         <h5>${item.title}</h5>
-                        <p class="text-muted">${item.details.length > 400 ? item.details.slice(0, 400) : item.details}</p>
-                        <p><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <p class="text-muted">${item.details.length > 500 ? item.details.slice(0, 500) : item.details}</p>
+                        <div class='d-flex align-items-center justify-content-evenly gap-5'>
+                            <div class='d-md-flex align-items-center gap-2'>
+                                <div>
+                                    <img style="width:70px; border-radius:50%" src="${item.author.img}"/>
+                                </div>
+                                <div>
+                                    <small>${item.author.name}</small><br/>
+                                    <small>${item.author.published_date}</small>
+                                </div>
+                            </div>
+                            <div>
+                           <i class="fa-regular fa-eye"></i>
+                           <span>${item.total_view}</span>
+                            </div>
+                            <div>
+                                 <i class="fa-solid fa-star"></i>
+                                 <i class="fa-solid fa-star"></i>
+                                 <i class="fa-solid fa-star"></i>
+                                 <i class="fa-solid fa-star"></i>
+                                 <i class="fa-solid fa-star"></i>
+                            </div>
+                            <div>
+                            <i class="fa-solid fa-arrow-right text-primary"></i>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -50,6 +77,7 @@ const displayCategories = (categoryItem) => {
     toggleSpinner(false)
 }
 
+// data not found error message is here
 const dataNotFound = (categoryContainer, categoryItem) => {
     const div = document.createElement('div');
     div.innerHTML = `
@@ -57,7 +85,9 @@ const dataNotFound = (categoryContainer, categoryItem) => {
     `
     categoryContainer.appendChild(div);
 };
-// spinner show and hide
+
+
+// toggle spinner function is here
 const toggleSpinner = isLoading => {
     const spinner = document.getElementById('spinner');
     if (isLoading) {
